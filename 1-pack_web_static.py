297c4004e4b5d205271cc81f1b7df132fcafae2d
untalinfo <racso1607@gmail.com>
2020-08-17 22:38:54 +0000

    task1 added from container

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+generates a .tgz archive from the contents of the web_static folder of your
+AirBnB Clone repo
+"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    try:
+        date_format = "%Y%m%d%H%M%S"
+        date = datetime.now()
+        create = date.strftime(date_format)
+        local("mkdir -p versions")
+        fileTgz = "web_static_{}.tgz".format(create)
+        local("tar -cvzf versions/{} web_static".format(fileTgz))
+        return fileTgz
+    except:
+        return None
